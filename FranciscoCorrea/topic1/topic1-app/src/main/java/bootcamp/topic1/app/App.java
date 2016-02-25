deaa77b34dc3918b486023f8b18c8d8e50af5aa6
cactus <correa.francisco.21@gmail.com>
2016-02-25 03:51:46 -0300

    Create and build project with Maven

---
+++ FranciscoCorrea/topic1/topic1-app/src/main/java/bootcamp/topic1/app/App.java
@@ -0,0 +1,13 @@
+package bootcamp.topic1.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
