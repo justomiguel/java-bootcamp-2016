fa4095db8ea546389ba01dd1160b7dbe7abe8489
Juan Ignacio Cavalieri <juanicav@gmail.com>
2016-02-25 10:53:28 -0300

    Files corresponding to the Topic1 of Java Bootcamp

---
+++ JuanIgnacioCavalieri/Topic1/src/test/java/Application.java
@@ -0,0 +1,16 @@
+package topic1EJ3;
+
+public class Application {
+	public static void main(String[] args) {
+
+		System.out.println(getTesting());
+
+	}
+
+	public static String getTesting() {
+
+		return "Testing Maven";
+
+	}
+
+}
