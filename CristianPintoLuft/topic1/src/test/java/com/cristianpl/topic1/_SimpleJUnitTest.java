8ab93a6ac75f03c35aa122ec522d92d07c046b66
Cristian <Cristian>
2016-02-29 16:58:22 -0300

    Added missing files

---
+++ CristianPintoLuft/topic1/src/test/java/com/cristianpl/topic1/_SimpleJUnitTest.java
@@ -0,0 +1,13 @@
+package com.cristianpl.topic1;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class _SimpleJUnitTest {
+	@Test
+	public void simpleTest() {
+		String name = "Cristian";
+		assertEquals("Cristian", name);
+	}
+}
